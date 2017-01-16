0248d2858320c88dd47e4fec46894926037aeb07
vahid-g <vahid87@gmail.com>
2017-01-15 16:53:54 -0800

    nothing

--- src/inex09/InexMsnExperiment.java
+++ src/inex09/InexMsnExperiment.java
@@ -3,6 +3,9 @@ package inex09;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 
 public class InexMsnExperiment {
@@ -10,10 +13,10 @@ public class InexMsnExperiment {
 	public static void main(String[] args) {
 
 		// buildIndex("/scratch/data-sets/inex_09/000", "data/index/inex09");
-		
 		List<MsnQuery> queries = InexQueryServices.loadMsnQueries(
 				"data/queries/msn/query_qid.csv",
 				"data/queries/msn/qid_qrel.csv");
+		
 		
 		List<MsnQueryResult> results = InexQueryServices.runMsnQueries(queries, "data/index/inex09");
 		try (FileWriter fw = new FileWriter("data/result/inex_msn.csv")) {
@@ -28,6 +31,37 @@ public class InexMsnExperiment {
 	public static void buildIndex(String datasetDir, String indexPath) {
 		List<String> allFiles = Utils.listFilesForFolder(new File(datasetDir));
 		InexIndexer.buildIndex(allFiles.toArray(new String[0]), indexPath);
+	}
+	
+	public static void experiment0() { // partitioning independent of related tuples
+		List<String> allFiles = Utils.listFilesForFolder(new File("???"));
+		Collections.shuffle(allFiles);
+		int partitionNo = 10;
+		ArrayList<String[]> partitions = Utils.partitionArray(
+				allFiles.toArray(new String[allFiles.size()]), partitionNo);
+		String prevExperiment = null;
+		
+		for (int i = 0; i < partitionNo; i++) {
+			System.out.println("iteration " + i);
+			Date index_t = new Date();
+			System.out.println("indexing ");
+			System.out.println("partition length: " + partitions.get(i).length);
+			InexIndexer.buildIndex(partitions.get(i), "???");
+			if (prevExperiment != null) {
+				System.out.println("updating index..");
+				InexIndexer.updateIndex("???", "???");
+			}
+			Date query_t = new Date();
+			prevExperiment = "???";
+			System.out.println("indexing time "
+					+ (query_t.getTime() - index_t.getTime()) / 60000 + "mins");
+			
+			System.out.println("querying time "
+					+ (new Date().getTime() - query_t.getTime()) / 60000
+					+ "mins");
+			
+			
+		}
 	}
 
 }
