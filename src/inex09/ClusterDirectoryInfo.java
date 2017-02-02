3d1e4233bc5c2a8bd34a8d86cddcdc6d5472115e
vahid-g <vahid87@gmail.com>
2017-02-01 20:01:16 -0800

    cluster dir

---
+++ src/inex09/ClusterDirectoryInfo.java
@@ -0,0 +1,14 @@
+package inex09;
+
+public class ClusterDirectoryInfo {
+
+	static final String CLUSTER_BASE = "/scratch/cluster-share/ghadakcv/";
+	static final String DATASET_PATH = CLUSTER_BASE + "inex_09";
+	static final String PATH_COUNT_FILE = CLUSTER_BASE + "data/file_count.csv";
+	static final String QUERY_QID_FILE_PATH = CLUSTER_BASE + "data/msn/query_qid.csv";
+	static final String QID_QREL_FILE_PATH = CLUSTER_BASE + "data/msn/qid_qrel.csv";
+	static final String LOCAL_INDEX_BASE = "/scratch/ghadakcv/index09/";
+	static final String GLOBAL_INDEX_BASE = "/scratch/cluster-share/ghadakcv/index/";
+	static final String RESULT_DIR = CLUSTER_BASE + "data/result/";
+
+}
