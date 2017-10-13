f912cb843d54862eeeec73fa0ed1c151f3f01b3b
vahid-g <vahid87@gmail.com>
2017-10-12 20:04:31 -0700

    python files updates and cleaned

--- python/explore_regr_models.py
+++ python/explore_regr_models.py
@@ -1,4 +1,5 @@
 import math
+import main
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -16,40 +17,8 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.neural_network import MLPRegressor
 
 df = pd.read_csv('../data/amazon/everything/features_labels.csv', header=None, low_memory=False)
+df, _ = prep_data('')
 print(df.describe())
-print('data size: ' + repr(df.shape))
-print('first row: \n' + repr(df.iloc[0, :]))
-print('column types: \n' + repr(df.dtypes))
-
-print('cleaning data..')
-print('removing IDs..')
-isbns = df.as_matrix([0])
-del(df[0])
-print('converting categorical values to numerical values')
-df[1] = df[1].astype('category')
-cat_cols = df.select_dtypes(['category']).columns
-df[cat_cols] = df[cat_cols].apply(lambda x: x.cat.codes)
-df[4] = df[4].str.split("[.']").str.get(0).astype('float')
-df = df.fillna(df.mean())
-
-print('data cleaning is done.')
-print('data size' + repr(df.shape))
-print('first row: \n' + repr(df.iloc[0, :]))
-
-'''
-print('preparing train/test data..')
-train_size = int(df.shape[0] * 0.75)
-X_train = df.iloc[:train_size,:-1]
-scaler = preprocessing.StandardScaler().fit(X_train)
-X = scaler.transform(df.iloc[:,:-1])
-y = df.iloc[:, -1:]
-
-X_test = scaler.transform(df.iloc[train_size:,:-1])
-y_train = df.iloc[:train_size,-1:]
-y_test = df.iloc[train_size:,-1:]
-trian_ids = isbns[:train_size]
-test_ids = isbns[train_size:]
-'''
 
 print('=== linear regression ===')
 regr = linear_model.LinearRegression()
@@ -62,13 +31,6 @@ print('r2 = %.2f' % cross_val_score(regr, df.iloc[:, :-1], df.iloc[:,-1:], cv=10
 'r2').mean())
 print('rmse = %.2f' % np.sqrt(-1 * cross_val_score(regr, df.iloc[:, :-1], df.iloc[:,-1:], cv=10, scoring =
 'neg_mean_squared_error')).mean())
-regr = make_pipeline(preprocessing.StandardScaler(), linear_model.LinearRegression())
-print('r2 = %.2f' % cross_val_score(regr, df.iloc[:, :-1], df.iloc[:,-1:], cv=10, scoring =
-'r2').mean())
-print('rmse = %.2f' % np.sqrt(-1 * cross_val_score(regr, df.iloc[:, :-1], df.iloc[:,-1:], cv=10, scoring =
-'neg_mean_squared_error')).mean())
-
-'''
 print('=== ridge ===')
 regr = linear_model.Ridge(alpha = .05)
 print(cross_val_score(regr, df.iloc[:, :-1], df.iloc[:,-1:], cv = 10).mean())
@@ -79,7 +41,7 @@ print('=== Poly Linear ===')
 regr = Pipeline([('poly', PolynomialFeatures(degree=2)), ('linear',
     linear_model.LinearRegression(fit_intercept=False))])
 print(cross_val_score(regr, df.iloc[:, :-1], df.iloc[:,-1:], cv = 10).mean())
-'''
+
 print('=== Decision Tree ===')
 regr = tree.DecisionTreeRegressor()
 print('r2 = %.2f' % cross_val_score(regr, df.iloc[:, :-1], df.iloc[:,-1:], cv = 10, scoring =
---
+++ python/main.py
@@ -0,0 +1,86 @@
+import math
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import linear_model
+from sklearn import preprocessing
+from sklearn import tree
+from sklearn.pipeline import Pipeline, make_pipeline
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import mean_squared_error, r2_score, make_scorer
+from sklearn.metrics import average_precision_score, accuracy_score
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.neural_network import MLPRegressor
+
+def prep_data(path):
+    df = pd.read_csv(path, header=None, low_memory=False)
+    print(df.describe())
+    print('data size: ' + repr(df.shape))
+    print('first row: \n' + repr(df.iloc[0, :]))
+    print('column types: \n' + repr(df.dtypes))
+    print('cleaning data..')
+    print('removing IDs..')
+    isbns = df.as_matrix([0])
+    del(df[0])
+    print('converting categorical values to numerical values')
+    df[1] = df[1].astype('category')
+    df[1] = df[1].cat.add_categories('UNK').fillna('UNK')
+    df[3] = df[3].astype('category')
+    df[3] = df[3].mask(df[3] == 'summ', 0)
+    df[3] = df[3].astype('float32')
+    df[4] = df[4].str.split("[.']").str.get(0)
+    df[4] = df[4].astype('category')
+    df[4] = df[4].cat.add_categories('UNK_2').fillna('UNK_2')
+    cat_cols = df.select_dtypes(['category']).columns
+    print('cat cols:' + repr(cat_cols))
+    df[cat_cols] = df[cat_cols].apply(lambda x: x.cat.codes)
+    print('column types: \n' + repr(df.dtypes))
+    df = df.fillna(df.mean())
+    print('data cleaning is done.')
+    print('data size' + repr(df.shape))
+    print('first row: \n' + repr(df.iloc[0, :]))
+    return df, isbns
+
+
+def main():
+    print('training..')
+    df, isbns = prep_data("../data/amazon/ml_files/trainset_bin.csv");
+    X = df.iloc[:,:-1]
+    y = df.iloc[:, -1]
+
+    regr = make_pipeline(preprocessing.StandardScaler(),
+            linear_model.LogisticRegression(class_weight = {0: 0.9, 1:0.1}))
+    print('acc = %.2f' % cross_val_score(regr, X, y, cv = 10, scoring = 'accuracy').mean())
+    print('fitting model..')
+    clf = regr.fit(X, y)
+
+    print('testing..')
+    df, isbns = prep_data('../data/amazon/ml_files/testset_bin.csv')
+    X = df.iloc[:,:-1]
+    y = df.iloc[:, -1:]
+    pred = clf.predict(X)
+    acc = accuracy_score(y, pred)
+    print('ACC: %.4f' % acc)
+    ap = average_precision_score(y, pred)
+    print('AP: %.4f' % ap)
+
+    print('predicting for all data..')
+    df, isbns = prep_data('../data/amazon/ml_files/features.csv')
+    #df.to_pickle('clean.pk1')
+    # pred = regr.predict(df)
+    prob = regr.predict_proba(df)
+    pred = prob[:,1] > 0.9
+    pred =np.reshape(pred, (len(pred), 1))
+    print(pred.shape)
+    print(isbns.shape)
+    final = np.concatenate((isbns, pred), 1)
+    print(final.shape)
+    print(final[0])
+    np.savetxt('output2.csv', final, fmt='%s,%s', delimiter=',')
+
+if __name__ == '__main__':
+    main()
