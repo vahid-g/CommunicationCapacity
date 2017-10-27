575a6026df9a0a50e14492aceb98ecd751317f7a
Vahid Ghadakchi <vahid87@gmail.com>
2017-10-26 21:31:22 -0700

    ExperimentQueryTest added

---
+++ test/query/ExperimentQueryTest.java
@@ -0,0 +1,14 @@
+package query;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ExperimentQueryTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
