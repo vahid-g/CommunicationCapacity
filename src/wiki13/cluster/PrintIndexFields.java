a38592900ef8491cd8a5b2528c3b13b2b4b65ef2
vahid-g <vahid87@gmail.com>
2018-01-27 12:52:49 -0800

    print index fields

---
+++ src/wiki13/cluster/PrintIndexFields.java
@@ -0,0 +1,31 @@
+package wiki13.cluster;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Iterator;
+
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.Fields;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.MultiFields;
+import org.apache.lucene.store.FSDirectory;
+
+public class PrintIndexFields {
+
+    public static void main(String[] args) {
+	String indexPath = WikiClusterPaths.INDEX_BASE
+		+ "wiki13_p50_w13/part_1";
+	try (FSDirectory directory = FSDirectory.open(Paths.get(indexPath));
+		IndexReader reader = DirectoryReader.open(directory)) {
+	    final Fields fields = MultiFields.getFields(reader);
+	    final Iterator<String> iterator = fields.iterator();
+	    while (iterator.hasNext()) {
+		final String field = iterator.next();
+		System.out.println(field);
+	    }
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
+    }
+
+}
