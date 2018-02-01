46b1feef512b5329b3b562dc3f2abcad16dbf7f7
vahid-g <vahid87@gmail.com>
2018-01-31 17:11:31 -0800

    finding max value of click counts for a partition

--- src/mslr/DatasetMaker.java
+++ src/mslr/DatasetMaker.java
@@ -1,26 +1,38 @@
 package mslr;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class DatasetMaker {
 
+    final static int MAX_PAGECOUNT = 2789632;
+
     public static void main(String[] args) {
+	int partitionCount = 10;
+	List<Integer> clickCountsList = loadSortedClickCountList();
+	for (double i = 1; i <= partitionCount; i++) {
+	    double partitionPercentage = i / partitionCount; 
+	    int threshold = clickCountsList.get((int)Math.floor(partitionPercentage * clickCountsList.size()));
+	    
+	}
+    }
+
+    public static List<Integer> loadSortedClickCountList() {
 	String dataFile = "test_data/mslr_sample.txt";
-	String outputFile = "test_data/mslr_sample.csv";
 	int urlIdCounter = 0;
-	// Java 8 version!
-	try (BufferedReader br = new BufferedReader(new FileReader(dataFile));
-		FileWriter fw = new FileWriter(new File(outputFile))) {
+	List<Integer> clickCounts = new ArrayList<Integer>();
+	try (BufferedReader br = new BufferedReader(new FileReader(dataFile))) {
+	    Map<String, Integer> urlClickCountMap = new HashMap<String, Integer>();
 	    String line;
+	    System.out.println("Building URL -> ClickCount Map");
 	    while ((line = br.readLine()) != null) {
 		String[] fields = line.split(" ");
 		if (fields.length < 138) {
@@ -29,34 +41,32 @@ public class DatasetMaker {
 		    continue;
 		}
 		urlIdCounter++;
-		// int numberOfSlashes = Integer.parseInt(fields[127]);
-		// int urlLength = Integer.parseInt(fields[128]);
-		// int inLinks = Integer.parseInt(fields[129]);
-		// int outLinks = Integer.parseInt(fields[130]);
-		// double pageRank = Double.parseDouble(fields[131]);
-		// double qualityScore = Double.parseDouble(fields[133]);
-		// int urlClickCount = Integer.parseInt(fields[136]);
 		List<String> fieldsList = new ArrayList<String>();
 		fieldsList.add(fields[127]);
 		fieldsList.add(fields[128]);
 		fieldsList.add(fields[129]);
 		fieldsList.add(fields[130]);
-		fieldsList.add(fields[131]);
-		fieldsList.add(fields[133]);
-		fieldsList.add(fields[135]);
-		fieldsList.add(fields[136]);
-		fieldsList = fieldsList.stream()
+		String urlID = fieldsList.stream()
 			.map(x -> x.substring(x.indexOf(':') + 1))
-			.collect(Collectors.toList());
-		String fieldsString = fieldsList.stream()
 			.collect(Collectors.joining(" "));
-		fw.write(urlIdCounter + " " + fieldsString + "\n");
+		Integer clickCount = Integer.parseInt(
+			fields[136].substring(fields[136].indexOf(':')));
+		if (urlClickCountMap.containsKey(urlID)) {
+		    urlClickCountMap.put(urlID,
+			    urlClickCountMap.get(urlID) + 1);
+		} else {
+		    urlClickCountMap.put(urlID, clickCount);
+		}
 	    }
+
+	    System.out.println("Sorting URLs");
+	    clickCounts.addAll(urlClickCountMap.values());
+	    Collections.sort(clickCounts, Collections.reverseOrder());
 	} catch (FileNotFoundException e) {
 	    e.printStackTrace();
 	} catch (IOException e) {
 	    e.printStackTrace();
 	}
+	return clickCounts;
     }
-
 }
