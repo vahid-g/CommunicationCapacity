a4484f68482afeb1c0ce863808dca0082974dcf5
vahid-g <vahid87@gmail.com>
2018-02-01 09:20:15 -0800

    MSLR first complete version

--- src/mslr/MslrExperiment.java
+++ src/mslr/MslrExperiment.java
@@ -18,27 +18,37 @@ public class MslrExperiment {
     final static int MAX_PAGECOUNT = 2789632;
 
     public static void main(String[] args) {
+	String querysetPath = args[1];
 	int partitionCount = 10;
-	List<Integer> clickCountsList = loadSortedClickCountList("test_data/mslr_sample.txt");
-	Map<Integer, List<MslrQueryResult>> qidResultMap = null;
-	try (BufferedReader br = new BufferedReader(new FileReader(""))) {
+	System.out.println("Loading sorted click counts..");
+	List<Integer> clickCountsList = loadSortedClickCountList(querysetPath);
+	System.out.println("Loading query results..");
+	Map<Integer, List<QueryResult>> qidResultMap = null;
+	try (BufferedReader br = new BufferedReader(
+		new FileReader(querysetPath))) {
 	    qidResultMap = loadQueryResults(br);
 	} catch (FileNotFoundException e) {
 	    e.printStackTrace();
 	} catch (IOException e) {
 	    e.printStackTrace();
 	}
-	try (FileWriter fw = new FileWriter("")) {
+	System.out.println("Computing query results..");
+	try (FileWriter fw = new FileWriter("mslr_result.csv")) {
 	    for (int qid : qidResultMap.keySet()) {
 		StringBuilder sb = new StringBuilder();
-		sb.append(qid + ",");
+		sb.append(qid);
 		for (double i = 1; i <= partitionCount; i++) {
 		    double partitionPercentage = i / partitionCount;
-		    int threshold = clickCountsList.get((int) Math.floor(partitionPercentage * clickCountsList.size()));
-		    List<MslrQueryResult> results = qidResultMap.get(qid);
-		    results.stream().filter(result -> result.clickCount < threshold).collect(Collectors.toList());
+		    int threshold = clickCountsList.get((int) Math
+			    .ceil(partitionPercentage * clickCountsList.size())
+			    - 1);
+		    System.out.println(threshold);
+		    List<QueryResult> results = qidResultMap.get(qid);
+		    results = results.stream()
+			    .filter(result -> result.clickCount >= threshold)
+			    .collect(Collectors.toList());
 		    double pk = precisionAtK(results, 10);
-		    sb.append(pk + ",");
+		    sb.append("," + pk);
 		}
 		fw.write(sb.toString() + "\n");
 	    }
@@ -48,10 +58,85 @@ public class MslrExperiment {
 
     }
 
-    private static double precisionAtK(List<MslrQueryResult> results, int k) {
+    // loads the click counts of URLs (trying to match duplicate URLs) and sorts
+    // them
+    protected static List<Integer> loadSortedClickCountList(String dataFile) {
+	int urlIdCounter = 0;
+	List<Integer> clickCounts = new ArrayList<Integer>();
+	try (BufferedReader br = new BufferedReader(new FileReader(dataFile))) {
+	    Map<String, Integer> urlClickCountMap = new HashMap<String, Integer>();
+	    String line;
+	    System.out.println("Building URL -> ClickCount Map");
+	    while ((line = br.readLine()) != null) {
+		String[] fields = line.split(" ");
+		if (fields.length < 138) {
+		    System.err.println(
+			    "Not enough fields for line: " + urlIdCounter);
+		    continue;
+		}
+		urlIdCounter++;
+		List<String> fieldsList = new ArrayList<String>();
+		fieldsList.add(fields[127]);
+		fieldsList.add(fields[128]);
+		fieldsList.add(fields[129]);
+		fieldsList.add(fields[130]);
+		String urlID = fieldsList.stream()
+			.map(x -> getValueOfKeyValueString(x))
+			.collect(Collectors.joining(" "));
+		Integer clickCount = Integer
+			.parseInt(getValueOfKeyValueString(fields[136]));
+		if (urlClickCountMap.containsKey(urlID)) {
+		    continue;
+		} else {
+		    urlClickCountMap.put(urlID, clickCount);
+		}
+	    }
+	    System.out.println("Sorting URLs");
+	    clickCounts.addAll(urlClickCountMap.values());
+	    Collections.sort(clickCounts, Collections.reverseOrder());
+	} catch (FileNotFoundException e) {
+	    e.printStackTrace();
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
+	return clickCounts;
+    }
+
+    // loads a map of QIDs to MslrQueryResult list
+    protected static Map<Integer, List<QueryResult>> loadQueryResults(
+	    BufferedReader br) throws IOException {
+	Map<Integer, List<QueryResult>> qidResultMap = new HashMap<Integer, List<QueryResult>>();
+	String line;
+	int previousQid = -1;
+	List<QueryResult> results = new ArrayList<QueryResult>();
+	while ((line = br.readLine()) != null) {
+	    String fields[] = line.split(" ");
+	    int rel = Integer.parseInt(getValueOfKeyValueString(fields[0]));
+	    int qid = Integer.parseInt(getValueOfKeyValueString(fields[1]));
+	    double bm = Double
+		    .parseDouble(getValueOfKeyValueString(fields[107]));
+	    int cc = Integer.parseInt(getValueOfKeyValueString(fields[136]));
+	    if (qid != previousQid && previousQid != -1) {
+		Collections.sort(results, new BM25Comparator());
+		qidResultMap.put(previousQid, results);
+		results = new ArrayList<QueryResult>();
+	    }
+	    results.add(new QueryResult(bm, rel, cc));
+	    previousQid = qid;
+	}
+	Collections.sort(results, new BM25Comparator());
+	qidResultMap.put(previousQid, results);
+	return qidResultMap;
+    }
+
+    protected static String getValueOfKeyValueString(String keyValue) {
+	return keyValue.substring(keyValue.indexOf(':') + 1);
+    }
+
+    protected static double precisionAtK(List<QueryResult> results, int k) {
 	int counter = 0;
 	double tp = 0;
-	for (MslrQueryResult result : results) {
+	for (QueryResult result : results) {
 	    if (counter >= k) {
 		break;
 	    }
@@ -62,47 +147,7 @@ public class MslrExperiment {
 	return tp / k;
     }
 
-    private static Map<Integer, List<MslrQueryResult>> loadQueryResults(BufferedReader br) throws IOException {
-	Map<Integer, List<MslrQueryResult>> qidResultMap = new HashMap<Integer, List<MslrQueryResult>>();
-	String line;
-	int previousQid = -1;
-	List<MslrQueryResult> results = new ArrayList<MslrQueryResult>();
-	while ((line = br.readLine()) != null) {
-	    String fields[] = line.split(" ");
-	    int rel = Integer.parseInt(fields[0]);
-	    int qid = Integer.parseInt(fields[1]);
-	    double bm = Double.parseDouble(fields[107]);
-	    int cc = Integer.parseInt(fields[136]);
-	    if (qid != previousQid && previousQid != -1) {
-		Collections.sort(results, new Comparator<MslrQueryResult>() {
-		    @Override
-		    public int compare(MslrQueryResult o1, MslrQueryResult o2) {
-			return (o1.bm25 > o2.bm25) ? 1 : 0;
-		    }
-		});
-		qidResultMap.put(previousQid, results);
-		results = new ArrayList<MslrQueryResult>();
-	    }
-	    results.add(new MslrQueryResult(bm, rel, cc));
-	    previousQid = qid;
-	}
-	return qidResultMap;
-    }
-
-    static class MslrQueryResult {
-	double bm25;
-	int rel;
-	int clickCount;
-
-	public MslrQueryResult(double bm25, int rel, int clickCount) {
-	    this.bm25 = bm25;
-	    this.rel = rel;
-	    this.clickCount = clickCount;
-	}
-    }
-
-    public static List<Integer> parseQueries() {
-	String dataFile = "test_data/mslr_sample.txt";
+    protected static List<Integer> parseQueries(String dataFile) {
 	int urlIdCounter = 0;
 	List<Integer> clickCounts = new ArrayList<Integer>();
 	try (BufferedReader br = new BufferedReader(new FileReader(dataFile))) {
@@ -112,7 +157,8 @@ public class MslrExperiment {
 	    while ((line = br.readLine()) != null) {
 		String[] fields = line.split(" ");
 		if (fields.length < 138) {
-		    System.err.println("Not enough fields for line: " + urlIdCounter);
+		    System.err.println(
+			    "Not enough fields for line: " + urlIdCounter);
 		    continue;
 		}
 		urlIdCounter++;
@@ -121,11 +167,14 @@ public class MslrExperiment {
 		fieldsList.add(fields[128]);
 		fieldsList.add(fields[129]);
 		fieldsList.add(fields[130]);
-		String urlID = fieldsList.stream().map(x -> x.substring(x.indexOf(':') + 1))
+		String urlID = fieldsList.stream()
+			.map(x -> x.substring(x.indexOf(':') + 1))
 			.collect(Collectors.joining(" "));
-		Integer clickCount = Integer.parseInt(fields[136].substring(fields[136].indexOf(':') + 1));
+		Integer clickCount = Integer.parseInt(
+			fields[136].substring(fields[136].indexOf(':') + 1));
 		if (urlClickCountMap.containsKey(urlID)) {
-		    urlClickCountMap.put(urlID, urlClickCountMap.get(urlID) + clickCount);
+		    urlClickCountMap.put(urlID,
+			    urlClickCountMap.get(urlID) + clickCount);
 		} else {
 		    // urlClickCountMap.put(urlID, clickCount);
 		    continue;
@@ -143,45 +192,22 @@ public class MslrExperiment {
 	return clickCounts;
     }
 
-    public static List<Integer> loadSortedClickCountList(String dataFile) {
-	int urlIdCounter = 0;
-	List<Integer> clickCounts = new ArrayList<Integer>();
-	try (BufferedReader br = new BufferedReader(new FileReader(dataFile))) {
-	    Map<String, Integer> urlClickCountMap = new HashMap<String, Integer>();
-	    String line;
-	    System.out.println("Building URL -> ClickCount Map");
-	    while ((line = br.readLine()) != null) {
-		String[] fields = line.split(" ");
-		if (fields.length < 138) {
-		    System.err.println("Not enough fields for line: " + urlIdCounter);
-		    continue;
-		}
-		urlIdCounter++;
-		List<String> fieldsList = new ArrayList<String>();
-		fieldsList.add(fields[127]);
-		fieldsList.add(fields[128]);
-		fieldsList.add(fields[129]);
-		fieldsList.add(fields[130]);
-		String urlID = fieldsList.stream().map(x -> x.substring(x.indexOf(':') + 1))
-			.collect(Collectors.joining(" "));
-		Integer clickCount = Integer.parseInt(fields[136].substring(fields[136].indexOf(':') + 1));
-		if (urlClickCountMap.containsKey(urlID)) {
-		    continue;
-		    // urlClickCountMap.put(urlID,
-		    // urlClickCountMap.get(urlID) + clickCount);
-		} else {
-		    urlClickCountMap.put(urlID, clickCount);
-		}
-	    }
+    protected static class QueryResult {
+	double bm25;
+	int rel;
+	int clickCount;
 
-	    System.out.println("Sorting URLs");
-	    clickCounts.addAll(urlClickCountMap.values());
-	    Collections.sort(clickCounts, Collections.reverseOrder());
-	} catch (FileNotFoundException e) {
-	    e.printStackTrace();
-	} catch (IOException e) {
-	    e.printStackTrace();
+	public QueryResult(double bm25, int rel, int clickCount) {
+	    this.bm25 = bm25;
+	    this.rel = rel;
+	    this.clickCount = clickCount;
 	}
-	return clickCounts;
+    }
+
+    protected static class BM25Comparator implements Comparator<QueryResult> {
+	@Override
+	public int compare(QueryResult o1, QueryResult o2) {
+	    return (o1.bm25 > o2.bm25) ? 1 : 0;
+	}
     }
 }
--- test/mslr/MslrExperimentTest.java
+++ test/mslr/MslrExperimentTest.java
@@ -2,16 +2,41 @@ package mslr;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.Test;
+
+import mslr.MslrExperiment.QueryResult;
 
 public class MslrExperimentTest {
 
     @Test
     public void testLoadSortedClickCountList() {
-	List<Integer> results = MslrExperiment.loadSortedClickCountList("test_data/mslr_sample.txt");
-	assertEquals(3, (int) results.get(0));
+	List<Integer> results = MslrExperiment
+		.loadSortedClickCountList("test_data/mslr_sample.txt");
+	assertEquals(10, (int) results.get(0));
+    }
+
+    @Test
+    public void testLoadQueryResults() throws IOException {
+	BufferedReader br = new BufferedReader(
+		new FileReader("test_data/mslr_sample.txt"));
+	Map<Integer, List<QueryResult>> qidResults = MslrExperiment
+		.loadQueryResults(br);
+	assertEquals(1, qidResults.keySet().size());
+	assertEquals(10, qidResults.get(1).size());
+    }
+
+    @Test
+    public void testGetValueOfKeyValueString() {
+	String keyValue = "key:value";
+	assertEquals("value",
+		MslrExperiment.getValueOfKeyValueString(keyValue));
+	assertEquals("", MslrExperiment.getValueOfKeyValueString(":"));
     }
 
 }
