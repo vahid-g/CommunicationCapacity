eac01906c0e9e569d4d512a19e1e8d289118e6e6
vahid-g <vahid87@gmail.com>
2018-03-27 15:13:59 -0700

    parallelized

--- src/stackoverflow/StackExperiment.java
+++ src/stackoverflow/StackExperiment.java
@@ -36,7 +36,7 @@ public class StackExperiment {
 	public static void main(String[] args) throws IOException, SQLException {
 		StackExperiment se = new StackExperiment();
 		LOGGER.log(Level.INFO, "retrieving queries..");
-		List<QuestionDAO> questions = se.loadQueriesWithBody();
+		List<QuestionDAO> questions = se.loadQueries(args[0], args[1]);
 		try (IndexReader reader = DirectoryReader.open(NIOFSDirectory.open(Paths.get("/data/ghadakcv/stack_index")))) {
 			IndexSearcher searcher = new IndexSearcher(reader);
 			Analyzer analyzer = new StandardAnalyzer();
@@ -46,45 +46,38 @@ public class StackExperiment {
 			for (QuestionDAO question : questions) {
 				try {
 					String queryText = question.text.replaceAll("[^a-zA-Z0-9 ]", " ").replaceAll("\\s+", " ");
-					// System.out.println("qid: " + question.id + " rel: " + question.answer);
-					// System.out.println(queryText);
 					Query query = parser.parse(queryText);
 					ScoreDoc[] hits = searcher.search(query, 200).scoreDocs;
-					// int counter = 0;
 					for (int i = 0; i < hits.length; i++) {
 						Document doc = searcher.doc(hits[i].doc);
 						if (doc.get(StackIndexer.ID_FIELD).equals(question.answer)) {
 							question.resultRank = i + 1;
 							break;
 						}
-						// if (counter++ < 3) {
-						// System.out.println("\t" + doc.get(StackIndexer.ID_FIELD));
-						// }
 					}
-					// System.out.println();
 				} catch (ParseException e) {
 					LOGGER.log(Level.SEVERE, "Couldn't parse query " + question.id);
 					LOGGER.log(Level.SEVERE, e.getMessage(), e);
 				}
 			}
 			LOGGER.log(Level.INFO, "writing results to file..");
-			try (FileWriter fw = new FileWriter(new File("stack_b.csv"))) {
+			try (FileWriter fw = new FileWriter(new File(args[0] + ".csv"))) {
 				for (QuestionDAO question : questions) {
 					if (question.resultRank != -1) {
-						fw.write(question.id + "," + 1.0 / question.resultRank + "\n");
+						fw.write(question.id + "," + question.answer + "," + 1.0 / question.resultRank + "\n");
 					}
 				}
 			}
 		}
 	}
 
-	List<QuestionDAO> loadQueries() throws IOException, SQLException {
+	List<QuestionDAO> loadQueries(String offset, String limit) throws IOException, SQLException {
 		DatabaseConnection dc = new DatabaseConnection(DatabaseType.STACKOVERFLOW);
 		Connection conn = dc.getConnection();
 		conn.setAutoCommit(false);
 		List<QuestionDAO> result = new ArrayList<QuestionDAO>();
 		String query = "select Id, Title, AcceptedAnswerId from stack_overflow.questions"
-				+ " where AcceptedAnswerId is not null limit 20000;";
+				+ " where AcceptedAnswerId is not null limit " + offset + "," + limit + ";";
 		try (Statement stmt = conn.createStatement()) {
 			stmt.setFetchSize(Integer.MIN_VALUE);
 			ResultSet rs = stmt.executeQuery(query);
