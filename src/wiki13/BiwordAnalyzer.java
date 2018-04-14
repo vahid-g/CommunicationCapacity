140cb408dbb6e2037b71fd9ef83b8da257565fe5
vahid-g <vahid87@gmail.com>
2018-04-13 17:25:47 -0700

    Biword analyzer added

---
+++ src/wiki13/BiwordAnalyzer.java
@@ -0,0 +1,19 @@
+package wiki13;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.LowerCaseFilter;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.Tokenizer;
+import org.apache.lucene.analysis.shingle.ShingleFilter;
+import org.apache.lucene.analysis.standard.StandardTokenizer;
+
+public class BiwordAnalyzer extends Analyzer {
+	@Override
+	protected TokenStreamComponents createComponents(String fieldName) {
+		Tokenizer source = new StandardTokenizer();
+		TokenStream filter = new LowerCaseFilter(source);
+		ShingleFilter sf = new ShingleFilter(filter);
+		sf.setOutputUnigrams(false);
+		return new TokenStreamComponents(source, sf);
+	}
+}
