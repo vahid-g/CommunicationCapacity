daba3ed034c16aa8c92b33118739fbbe156a352b
vahid-g <vahid87@gmail.com>
2018-04-13 17:16:17 -0700

    analyzer argument added

--- src/indexing/InexDatasetIndexer.java
+++ src/indexing/InexDatasetIndexer.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
@@ -30,7 +31,13 @@ public class InexDatasetIndexer {
 	}
 
 	public void buildIndex(List<InexFile> list, String indexPath, Similarity similarity) {
-		indexWriterConfig = new IndexWriterConfig(new StandardAnalyzer());
+		try (Analyzer analyzer = new StandardAnalyzer()) {
+			buildIndex(list, indexPath, similarity, analyzer);
+		}
+	}
+
+	public void buildIndex(List<InexFile> list, String indexPath, Similarity similarity, Analyzer analyzer) {
+		indexWriterConfig = new IndexWriterConfig(analyzer);
 		indexWriterConfig.setOpenMode(OpenMode.CREATE);
 		indexWriterConfig.setRAMBufferSizeMB(1024.00);
 		indexWriterConfig.setSimilarity(similarity);
