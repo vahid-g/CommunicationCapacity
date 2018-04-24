2b30ee901726981fda200c640eed77b90b239d38
Vahid Ghadakchi <vahid87@gmail.com>
2018-04-23 20:35:42 -0700

    print index

--- src/indexing/PrintFieldTerms.java
+++ src/indexing/PrintFieldTerms.java
@@ -2,10 +2,8 @@ package indexing;
 
 import java.io.IOException;
 import java.nio.file.Paths;
-import java.util.Iterator;
 
 import org.apache.lucene.index.DirectoryReader;
-import org.apache.lucene.index.Fields;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.MultiFields;
 import org.apache.lucene.index.Terms;
@@ -13,30 +11,26 @@ import org.apache.lucene.index.TermsEnum;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.BytesRef;
 
-public class PrintFirstIndexTerm {
+public class PrintFieldTerms {
 
 	public static void main(String[] args) {
 		String indexPath = args[0];
+		String field = args[1];
 		try (FSDirectory directory = FSDirectory.open(Paths.get(indexPath));
 				IndexReader reader = DirectoryReader.open(directory)) {
-			final Fields fields = MultiFields.getFields(reader);
-			final Iterator<String> iterator = fields.iterator();
-			while (iterator.hasNext()) {
-				final String field = iterator.next();
-				System.out.println("filed " + field);
-				final Terms terms = MultiFields.getTerms(reader, field);
-				final TermsEnum it = terms.iterator();
-				BytesRef term = it.next();
-				int counter = 0;
-				while (term != null) {
-					System.out.println(term.utf8ToString());
-					term = it.next();
-					if (counter++ > 10) break;
-				}
-			}
-
+			printFieldTerms(field, reader);
 		} catch (IOException e) {
 			e.printStackTrace();
+		}
+	}
+
+	static void printFieldTerms(String field, IndexReader reader) throws IOException {
+		final Terms terms = MultiFields.getTerms(reader, field);
+		final TermsEnum it = terms.iterator();
+		BytesRef term = it.next();
+		while (term != null) {
+			System.out.println(term.utf8ToString());
+			term = it.next();
 		}
 	}
 
---
+++ src/indexing/PrintIndexTerms.java
@@ -0,0 +1,32 @@
+package indexing;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Iterator;
+
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.Fields;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.MultiFields;
+import org.apache.lucene.store.FSDirectory;
+
+public class PrintIndexTerms {
+
+	public static void main(String[] args) {
+		String indexPath = args[0];
+		try (FSDirectory directory = FSDirectory.open(Paths.get(indexPath));
+				IndexReader reader = DirectoryReader.open(directory)) {
+			final Fields fields = MultiFields.getFields(reader);
+			final Iterator<String> iterator = fields.iterator();
+			while (iterator.hasNext()) {
+				final String field = iterator.next();
+				System.out.println("filed " + field);
+				PrintFieldTerms.printFieldTerms(field, reader);
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
