1171829df066f097045fb6932d42663f4ddea2ae
vahid-g <vahid87@gmail.com>
2018-05-25 18:14:10 -0700

    nothing

---
+++ src/stackoverflow/StackQueryAnswer.java
@@ -0,0 +1,15 @@
+package stackoverflow;
+
+public class StackQueryAnswer {
+	
+	QuestionDAO question;
+	
+	public double rrank = 0;
+	
+	public double recall = 0;
+
+	public StackQueryAnswer(QuestionDAO question) {
+		this.question = question;
+	}
+	
+}
