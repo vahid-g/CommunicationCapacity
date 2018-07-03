afcb0ef4bffc038781f220e24fce889b8383c4bd
vahid-g <vahid87@gmail.com>
2018-07-02 17:53:44 -0700

    find cache

---
+++ src/irstyle/WikiFindCache.java
@@ -0,0 +1,68 @@
+package irstyle;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.store.FSDirectory;
+
+import indexing.GeneralIndexer;
+import query.ExperimentQuery;
+import query.QueryResult;
+import query.QueryServices;
+import wiki13.WikiFilesPaths;
+
+public class WikiFindCache {
+
+	public static int TOPDOC_COUNTS = 100;
+
+	public static void main(String[] args) throws IOException, ParseException {
+
+		WikiFilesPaths paths = WikiFilesPaths.getMaplePaths();
+		List<ExperimentQuery> queries = QueryServices.loadMsnQueries(paths.getMsnQueryFilePath(),
+				paths.getMsnQrelFilePath());
+		Collections.shuffle(queries, new Random(1));
+		queries = queries.subList(0, 50);
+		String tableName = "tbl_article_09";
+		for (int i = 1; i <= 100; i += 10) {
+			String indexPath = "/data/ghadakcv/wikipedia/tbl_" + tableName + "/" + i;
+			List<QueryResult> queryResults = new ArrayList<QueryResult>();
+			try (IndexReader reader = DirectoryReader.open(FSDirectory.open(Paths.get(indexPath)))) {
+				IndexSearcher searcher = new IndexSearcher(reader);
+				QueryParser qp = new QueryParser(WikiTableIndexer.TEXT_FIELD, new StandardAnalyzer());
+				for (ExperimentQuery q : queries) {
+					QueryResult iqr = new QueryResult(q);
+					Query query = qp.parse(q.getText());
+					ScoreDoc[] scoreDocHits = searcher.search(query, TOPDOC_COUNTS).scoreDocs;
+					for (int j = 0; j < Math.min(TOPDOC_COUNTS, scoreDocHits.length); j++) {
+						Document doc = reader.document(scoreDocHits[j].doc);
+						String docId = doc.get(GeneralIndexer.DOCNAME_ATTRIB);
+						String docTitle = doc.get(GeneralIndexer.TITLE_ATTRIB);
+						iqr.addResult(docId, docTitle);
+					}
+					queryResults.add(iqr);
+				}
+			}
+			double mrr = 0;
+			for (QueryResult qr : queryResults) {
+				mrr += qr.mrr();
+			}
+			mrr = mrr / mrr / queryResults.size();
+			System.out.println("index: " + i + " mrr = " + mrr);
+		}
+
+	}
+
+}
