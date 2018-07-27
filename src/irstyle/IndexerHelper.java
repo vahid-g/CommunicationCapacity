aa932099dad00faedbeffe6142d1f85ed8616d90
vahid-g <vahid87@gmail.com>
2018-07-27 14:31:31 -0700

    cache build 1.0

--- src/irstyle/IndexerHelper.java
+++ src/irstyle/IndexerHelper.java
@@ -3,6 +3,7 @@ package irstyle;
 import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field.Store;
--- src/irstyle/RunBuildCache.java
+++ src/irstyle/RunBuildCache.java
@@ -12,6 +12,9 @@ import java.util.List;
 
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
+import org.apache.lucene.document.StoredField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.document.Field.Store;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
@@ -37,13 +40,13 @@ public class RunBuildCache {
 
 	public static void main(String[] args) throws IOException, SQLException, ParseException {
 		try (DatabaseConnection dc = new DatabaseConnection(DatabaseType.WIKIPEDIA)) {
-			Connection conn = dc.getConnection();
 			String tableName = "tbl_article_wiki13";
 			String[] textAttribs = new String[] { "title", "text" };
 			String cacheTable = "sub_" + tableName.substring(4);
 			String indexPath = "/data/ghadakcv/wikipedia/" + cacheTable;
-			int pageSize = 10;
-			try (Statement stmt = dc.getConnection().createStatement()) {
+			int pageSize = 100000;
+			Connection conn = dc.getConnection();
+			try (Statement stmt = conn.createStatement()) {
 				stmt.execute("drop table if exists " + cacheTable + ";");
 				stmt.execute("create table " + cacheTable + " as select id from " + tableName
 						+ " order by popularity desc limit " + pageSize + ";");
@@ -52,7 +55,6 @@ public class RunBuildCache {
 			String selectTemplate = "select * from " + tableName + " order by popularity desc limit ?, " + pageSize
 					+ ";";
 			String insertTemplate = "insert into " + cacheTable + " (id) values (?);";
-			int offset = pageSize;
 			IndexWriterConfig config = new IndexWriterConfig(new StandardAnalyzer());
 			config.setSimilarity(new BM25Similarity());
 			config.setRAMBufferSizeMB(1024);
@@ -62,46 +64,66 @@ public class RunBuildCache {
 			List<ExperimentQuery> queries = null;
 			queries = QueryServices.loadMsnQueries(paths.getMsnQueryFilePath(), paths.getMsnQrelFilePath());
 			queries = queries.subList(0, 50);
-			int topDocs = 20;
+			int topDocs = 100;
 			try (IndexWriter indexWriter = new IndexWriter(directory, config)) {
 				try (PreparedStatement selectStmt = conn.prepareStatement(selectTemplate);
 						PreparedStatement insertStmt = conn.prepareStatement(insertTemplate)) {
 					double prevAcc = 0;
 					double acc = 0;
+					int offset = pageSize;
+					int loop = 1;
 					while (true) {
+						System.out.println("Iteration " + loop++);
 						selectStmt.setInt(1, offset);
 						offset += pageSize;
 						ResultSet rs = selectStmt.executeQuery();
+						// updating cache and index
+						System.out.println("  reading new cache data..");
+						List<Document> docs = new ArrayList<Document>();
 						while (rs.next()) {
 							int id = rs.getInt("id");
 							insertStmt.setInt(1, id);
 							insertStmt.addBatch();
-							String text = rs.getString("title") + rs.getString("text");
-							IndexerHelper.indexRS("id", textAttribs, indexWriter, rs);
+							String text = "";
+							for (String attrib : textAttribs) {
+								text += rs.getString(attrib);
+							}
+							Document doc = new Document();
+							doc.add(new StoredField("id", id));
+							doc.add(new TextField("text", text, Store.NO));
+							docs.add(doc);
 						}
+						System.out.println("  updating cache table..");
 						insertStmt.executeBatch();
+						System.out.println("  updating cache index..");
+						indexWriter.addDocuments(docs);
+						indexWriter.flush();
+						// indexWriter.commit();
 						// test partition!
+						System.out.println("  testing new cache..");
 						List<QueryResult> queryResults = new ArrayList<QueryResult>();
 						try (IndexReader reader = DirectoryReader.open(FSDirectory.open(Paths.get(indexPath)))) {
+							System.out.println("  index size: " + reader.numDocs());
 							IndexSearcher searcher = new IndexSearcher(reader);
-							QueryParser qp = new QueryParser(IndexerHelper.TEXT_FIELD, new StandardAnalyzer());
+							QueryParser qp = new QueryParser("text", new StandardAnalyzer());
 							for (ExperimentQuery q : queries) {
 								QueryResult result = new QueryResult(q);
 								Query query = qp.parse(QueryParser.escape(q.getText()));
 								ScoreDoc[] scoreDocHits = searcher.search(query, topDocs).scoreDocs;
 								for (int j = 0; j < Math.min(topDocs, scoreDocHits.length); j++) {
 									Document doc = reader.document(scoreDocHits[j].doc);
-									String docId = doc.get(IndexerHelper.ID_FIELD);
+									String docId = doc.get("id");
 									result.addResult(docId, "no title");
 								}
 								queryResults.add(result);
 							}
-							acc = effectiveness(queryResults);
-							if (acc < prevAcc) {
-								break;
-							}
-							prevAcc = acc;
 						}
+						acc = effectiveness(queryResults);
+						System.out.println("  new accuracy = " + acc);
+						if (acc < prevAcc) {
+							break;
+						}
+						prevAcc = acc;
 					}
 				}
 			}
