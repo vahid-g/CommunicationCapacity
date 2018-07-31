166ee2e4ac277406ddf69805cd74e5b2f1127d06
vahid-g <vahid87@gmail.com>
2018-07-31 11:28:47 -0700

    params

--- src/irstyle/Params.java
+++ src/irstyle/Params.java
@@ -3,9 +3,9 @@ package irstyle;
 public class Params {
 
 	static boolean DEBUG = false;
-	static int maxCNsize = 20;
+	static int maxCNsize = 5;
 	static int numExecutions = 1;
-	static int N = 5;
+	static int N = 20;
 	static boolean allKeywInResults = false;
 
 }
