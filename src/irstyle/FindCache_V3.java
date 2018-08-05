cc8e58226c8dba3a5d9682c783d8cc51be773a6c
Vahid Ghadakchi <vahid87@gmail.com>
2018-08-05 16:20:06 -0700

    V_3 first version

--- src/irstyle/FindCache_V3.java
+++ src/irstyle/FindCache_V3.java
@@ -8,9 +8,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
+import java.util.Set;
 import java.util.Vector;
 
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
@@ -36,8 +38,6 @@ import query.QueryServices;
 
 public class FindCache_V3 {
 
-	private static final double popSum[] = { 15440314886.0, 52716653460.0, 1407925741807.0 };
-
 	public static void main(String[] args) throws Exception {
 		List<String> argList = Arrays.asList(args);
 		List<ExperimentQuery> queries = null;
@@ -53,57 +53,67 @@ public class FindCache_V3 {
 		queries = queries.subList(0, 10);
 		try (DatabaseConnection dc = new DatabaseConnection(DatabaseType.WIKIPEDIA)) {
 			String[] tableNames = new String[] { "tbl_article_wiki13", "tbl_image_pop", "tbl_link_pop" };
+			String[] shortTableNames = new String[] { "a", "i", "l" };
 			String[][] textAttribs = new String[][] { { "title", "text" }, { "src" }, { "url" } };
 			Connection conn = dc.getConnection();
-			String[] cacheTables = new String[tableNames.length];
-			String[] selectTemplates = new String[tableNames.length];
+			conn.setAutoCommit(false);
+			String[] cacheTableNames = new String[tableNames.length];
+			String[] currentTupleSelectSql = new String[tableNames.length];
 			String[] insertTemplates = new String[tableNames.length];
-			String[] indexPaths = new String[tableNames.length];
-			RAMDirectory[] ramDir = new RAMDirectory[tableNames.length];
-			int[] pageSize = { 119450, 11830, 97663 };
-			// int[] pageSize = { 100000, 100000, 100000 };
 			double[] sizes = { 11945034, 1183070, 9766351 };
+			String[] joinMaxSelectTemplate = new String[tableNames.length];
+			String[] joinSelectTemplate = new String[tableNames.length];
+			String from = " from tbl_article_wiki13 a, tbl_article_image_09 ai, tbl_image_pop i, "
+					+ "tbl_article_link_09 al, tbl_link_pop l";
+			String whereTemplate = " where a.id = ai.article_id and ai.image_id = i.id and a.id = al.article_id "
+					+ "and al.link_id = l.id and ID_PLACE_HOLDER = ?";
+			String max = " max(least(a.popularity, i.popularity, l.popularity)) m";
+			String groupBy = " group by ID_PLACE_HOLDER ;";
+			String joinMaxSelectPrefix = "select ID_PLACE_HOLDER," + max + from + whereTemplate + groupBy;
+			String joinSelectPrefix = "select a.id, a.title, a.text, i.id, i.src, l.id, l.link" + from + whereTemplate;
+			PreparedStatement selectSt[] = new PreparedStatement[tableNames.length];
+			PreparedStatement insertSt[] = new PreparedStatement[tableNames.length];
+			PreparedStatement joinMaxSelectSt[] = new PreparedStatement[tableNames.length];
+			PreparedStatement joinSelectSt[] = new PreparedStatement[tableNames.length];
+			RAMDirectory[] ramDir = new RAMDirectory[tableNames.length];
 			IndexWriterConfig[] config = new IndexWriterConfig[tableNames.length];
+			IndexWriter indexWriters[] = new IndexWriter[tableNames.length];
 			for (int i = 0; i < tableNames.length; i++) {
-				cacheTables[i] = "tmp_" + tableNames[i].substring(4);
+				// prepare cache tables
+				cacheTableNames[i] = "tmp_" + tableNames[i].substring(4);
 				try (Statement stmt = conn.createStatement()) {
-					stmt.execute("drop table if exists " + cacheTables[i] + ";");
+					stmt.execute("drop table if exists " + cacheTableNames[i] + ";");
 					stmt.execute(
-							"create table " + cacheTables[i] + " as select id from " + tableNames[i] + " limit 0;");
-					stmt.execute("create index id on " + cacheTables[i] + "(id);");
+							"create table " + cacheTableNames[i] + " as select id from " + tableNames[i] + " limit 0;");
+					stmt.execute("create index id on " + cacheTableNames[i] + "(id);");
 				}
-				selectTemplates[i] = "select * from " + tableNames[i] + " order by popularity desc limit ?, "
-						+ pageSize[i] + ";";
-				insertTemplates[i] = "insert into " + cacheTables[i] + " (id) values (?);";
-				indexPaths[i] = "/data/ghadakcv/wikipedia/" + cacheTables[i];
+				// prepare prepared statements
+				// TODO: rename popularity column
+				currentTupleSelectSql[i] = "select id from " + tableNames[i] + " order by popularity desc;";
+				insertTemplates[i] = "insert into " + cacheTableNames[i] + " (id) values (?);";
+				joinMaxSelectTemplate[i] = joinMaxSelectPrefix.replaceAll("ID_PLACE_HOLDER", shortTableNames[i] + ".id");
+				joinSelectTemplate[i] = joinSelectPrefix.replaceAll("ID_PLACE_HOLDER", shortTableNames[i] + ".id");
+				selectSt[i] = conn.prepareStatement(currentTupleSelectSql[i]);
+				insertSt[i] = conn.prepareStatement(insertTemplates[i]);
+				joinMaxSelectSt[i] = conn.prepareStatement(joinMaxSelectTemplate[i]);
+				joinSelectSt[i] = conn.prepareStatement(joinSelectTemplate[i]);
+				// prepare index writer
 				config[i] = new IndexWriterConfig(new StandardAnalyzer());
 				config[i].setSimilarity(new BM25Similarity());
 				config[i].setRAMBufferSizeMB(1024);
 				config[i].setOpenMode(OpenMode.CREATE);
-
-			}
-			IndexWriter indexWriters[] = new IndexWriter[tableNames.length];
-			PreparedStatement selectSt[] = new PreparedStatement[tableNames.length];
-			PreparedStatement insertSt[] = new PreparedStatement[tableNames.length];
-			for (int i = 0; i < tableNames.length; i++) {
-				// indexWriters[i] = new IndexWriter(FSDirectory.open(Paths.get(indexPaths[i])),
-				// config[i]);
 				ramDir[i] = new RAMDirectory();
 				indexWriters[i] = new IndexWriter(ramDir[i], config[i]);
 				indexWriters[i].commit();
-				selectSt[i] = conn.prepareStatement(selectTemplates[i]);
-				insertSt[i] = conn.prepareStatement(insertTemplates[i]);
 			}
 			double prevAcc = 0;
 			double acc = 0;
 			double bestAcc = 0;
-			int[] offset = { 0, 0, 0 };
-			int[] bestOffset = { 0, 0, 0 };
 			int loop = 1;
 			JDBCaccess jdbcacc = IRStyleKeywordSearch.jdbcAccess();
-			String articleTable = cacheTables[0];
-			String imageTable = cacheTables[1];
-			String linkTable = cacheTables[2];
+			String articleTable = cacheTableNames[0];
+			String imageTable = cacheTableNames[1];
+			String linkTable = cacheTableNames[2];
 			String articleImageTable = "tbl_article_image_09";
 			String articleLinkTable = "tbl_article_link_09";
 			String schemaDescription = "5 " + articleTable + " " + articleImageTable + " " + imageTable + " "
@@ -113,66 +123,73 @@ public class FindCache_V3 {
 			Vector<Relation> relations = IRStyleKeywordSearch.createRelations(articleTable, imageTable, linkTable,
 					articleImageTable, articleLinkTable, jdbcacc.conn);
 			IRStyleKeywordSearch.dropTupleSets(jdbcacc, relations);
-			List<List<Document>> docsList = new ArrayList<List<Document>>();
-			int[] lastPopularity = new int[tableNames.length];
-			int[] lastPopularTupleId = new int[tableNames.length];
-			String[] selectStatement = new String[tableNames.length];
-			String from = " from tbl_article_wiki13 a, tbl_article_image_09 ai, tbl_image_pop i, "
-					+ "tbl_article_link_09 al, tbl_link_pop l";
-			String whereTemplate = " where a.id = ia.article_id and ia.image_id = i.id and a.id = al.article_id "
-					+ "and al.link_id = l.id and a.id = ?";
-			String max = " max(least(a.popularity, i.pop, l.pop))";
-			String groupBy = " group by ? ;";
-			String joinSelectTemplate = " select ?," + max + from + whereTemplate + groupBy;
-			selectStatement[0] = joinSelectTemplate.replaceAll("?", "a.id");
-			selectStatement[1] = joinSelectTemplate.replaceAll("?", "i.id");
-			selectStatement[2] = joinSelectTemplate.replaceAll("?", "l.id");
+			int[] currentMaxPopularity = new int[tableNames.length];
+			int[] currentTupleId = new int[tableNames.length];
+			ResultSet[] currentTupleRS = new ResultSet[tableNames.length];
+			List<Set<Integer>> cachedTuplesList = new ArrayList<Set<Integer>>();
 			for (int i = 0; i < tableNames.length; i++) {
-				selectSt[i].setInt(1, offset[i]);
-				ResultSet rs = selectSt[i].executeQuery();
-				List<Document> docs = new ArrayList<Document>();
-				while (rs.next()) {
-					int id = rs.getInt("id");
-					String text = "";
-					for (String attrib : textAttribs[i]) {
-						text += rs.getString(attrib);
-					}
-					Document doc = new Document();
-					doc.add(new StoredField("id", id));
-					doc.add(new TextField("text", text, Store.NO));
-					docs.add(doc);
-					lastPopularity[i] = rs.getInt("popularity");
-					lastPopularTupleId[i] = id;
-				}
-				docsList.add(docs);
+				cachedTuplesList.add(new HashSet<Integer>());
+				currentTupleRS[i] = selectSt[i].executeQuery();
+				currentTupleRS[i].next();
+				currentTupleId[i] = currentTupleRS[i].getInt("id");
+				joinMaxSelectSt[i].setInt(1, currentTupleId[i]);
+				ResultSet maxPopularityRS = joinMaxSelectSt[i].executeQuery();
+				maxPopularityRS.next();
+				currentMaxPopularity[i] = maxPopularityRS.getInt("m");
 			}
-			
 			while (true) {
 				System.out.println("Iteration " + loop++);
-				double mPopularity = 0;
+				System.out.println("  picking max popularity..");
+				double maxPopularity = 0;
 				int m = -1;
-				for (int i = 0; i < lastPopularTupleId.length; i++) {
-					// compute popularity of the join
-					int pop = 0;
-					if (pop > mPopularity) {
-						mPopularity = pop;
+				for (int i = 0; i < currentTupleId.length; i++) {
+					if (currentMaxPopularity[i] > maxPopularity) {
+						maxPopularity = currentMaxPopularity[i];
 						m = i;
 					}
 				}
-
-				System.out.println("  Selected table = " + tableNames[m] + " with popularity = " + mPopularity);
-				List<Document> docs = docsList.get(m);
-				System.out.println("  reading new cache data..");
-				for (Document doc : docs) {
-					insertSt[m].setInt(1, Integer.parseInt(doc.get("id")));
-					insertSt[m].addBatch();
+				System.out.println("  selected table = " + tableNames[m] + " with popularity = " + maxPopularity);
+				joinSelectSt[m].setInt(1, currentTupleId[m]);
+				ResultSet joinRS = joinSelectSt[m].executeQuery();
+				List<List<Document>> docs = new ArrayList<List<Document>>();
+				for (int i = 0; i < docs.size(); i++) {
+					docs.add(new ArrayList<Document>());
 				}
-				System.out.println("  updating cache table..");
-				insertSt[m].executeBatch();
-				System.out.println("  updating cache index..");
-				indexWriters[m].addDocuments(docs);
-				indexWriters[m].flush();
-				indexWriters[m].commit();
+				System.out.println("  updating caches..");
+				while (joinRS.next()) {
+					for (int i = 0; i < tableNames.length; i++) {
+						Integer id = joinRS.getInt(shortTableNames[i] + ".id");
+						if (!cachedTuplesList.get(i).contains(id)) {
+							cachedTuplesList.get(i).add(id);
+							insertSt[i].setInt(1, id);
+							insertSt[i].addBatch();
+							String text = "";
+							for (String attrib : textAttribs[i]) {
+								text += joinRS.getString(attrib);
+							}
+							Document doc = new Document();
+							doc.add(new StoredField("id", id));
+							doc.add(new TextField("text", text, Store.NO));
+							docs.get(i).add(doc);
+						}
+					}
+				}
+				for (int i = 0; i < tableNames.length; i++) {
+					insertSt[i].executeBatch();
+					indexWriters[i].addDocuments(docs.get(i));
+					indexWriters[i].flush();
+					indexWriters[i].commit();
+				}
+				conn.commit();
+				System.out.println("  sizes so far: "
+						+ cachedTuplesList.stream().map(l -> l.size() + "").reduce("", String::concat));
+				// updating pointers
+				currentTupleRS[m].next();
+				currentTupleId[m] = currentTupleRS[m].getInt("id");
+				joinMaxSelectSt[m].setInt(1, currentTupleId[m]);
+				ResultSet maxPopularityRS = joinMaxSelectSt[m].executeQuery();
+				maxPopularityRS.next();
+				currentMaxPopularity[m] = maxPopularityRS.getInt("m");
 				// test partition!
 				System.out.println("  testing new cache..");
 				List<IRStyleQueryResult> queryResults = new ArrayList<IRStyleQueryResult>();
@@ -198,50 +215,29 @@ public class FindCache_V3 {
 					System.out.println("  new accuracy = " + acc);
 
 				}
-				offset[m] += pageSize[m];
-				System.out.println("  current offsets: " + Arrays.toString(offset));
 				if (acc > bestAcc) {
 					bestAcc = acc;
-					bestOffset = offset.clone();
 				}
 				if ((acc - prevAcc) > 0.05) {
 					System.out.println("  time to break;");
 					break;
 				}
 				prevAcc = acc;
-				if (lastPopularity[0] == -1 && lastPopularity[1] == -1 && lastPopularity[-2] == -1) {
+				if (currentMaxPopularity[0] == -1 && currentMaxPopularity[1] == -1 && currentMaxPopularity[-2] == -1) {
 					break;
 				}
-				// update buffer
-				selectSt[m].setInt(1, offset[m]);
-				ResultSet rs = selectSt[m].executeQuery();
-				lastPopularity[m] = -1;
-				docs = new ArrayList<Document>();
-				while (rs.next()) {
-					int id = rs.getInt("id");
-					String text = "";
-					for (String attrib : textAttribs[m]) {
-						text += rs.getString(attrib);
-					}
-					Document doc = new Document();
-					doc.add(new StoredField("id", id));
-					doc.add(new TextField("text", text, Store.NO));
-					docs.add(doc);
-					lastPopularity[m] = rs.getInt("popularity");
-				}
-				docsList.remove(m);
-				docsList.add(m, docs);
 			}
-			System.out.println("Offsets for articles, images, links = " + Arrays.toString(bestOffset));
-			double[] percent = new double[bestOffset.length];
-			for (int i = 0; i < bestOffset.length; i++) {
-				percent[i] = bestOffset[i] / sizes[i];
+			double[] percent = new double[tableNames.length];
+			for (int i = 0; i < cachedTuplesList.size(); i++) {
+				percent[i] = cachedTuplesList.get(i).size() / sizes[i];
 			}
 			System.out.println("Best found sizes = " + Arrays.toString(percent));
 			for (int i = 0; i < tableNames.length; i++) {
 				indexWriters[i].close();
 				selectSt[i].close();
 				insertSt[i].close();
+				joinMaxSelectSt[i].close();
+				joinSelectSt[i].close();
 			}
 		}
 	}
