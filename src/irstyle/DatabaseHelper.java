ee5d68285da96444c878f365d78b5ae1c81109d6
Vahid Ghadakchi <vahid87@gmail.com>
2018-08-18 15:08:37 -0700

    new query added

--- src/irstyle/DatabaseHelper.java
+++ src/irstyle/DatabaseHelper.java
@@ -1,9 +1,11 @@
 package irstyle;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Set;
 
 public class DatabaseHelper {
 
@@ -18,5 +20,21 @@ public class DatabaseHelper {
 		}
 		return count;
 	}
-	
+
+	static int relCounts(Connection conn, Set<String> ids) throws SQLException {
+		int count = 0;
+		String sql = " select count(*) as count from tbl_article_wiki13 a join table_article_image_09 ai on"
+				+ " a.id = ai.article_id join table_article_link_09 al on a.id = al.article_id" + " where a.id = ?;";
+		try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+			for (String id : ids) {
+				stmt.setInt(1, Integer.parseInt(id));
+				ResultSet rs = stmt.executeQuery();
+				if (rs.next()) {
+					count += rs.getInt("count");
+				}
+			}
+		}
+		return count;
+	}
+
 }
