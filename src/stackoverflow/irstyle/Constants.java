4a1f9edf5edc31cbf9206948f8ebc008a6441acf
Vahid Ghadakchi <vahid87@gmail.com>
2018-08-20 20:20:32 -0700

    stack constants added

---
+++ src/stackoverflow/irstyle/Constants.java
@@ -0,0 +1,11 @@
+package stackoverflow.irstyle;
+
+public class Constants {
+
+	public static String[] tableName = { "answers_s_2", "tags_pop", "comments_pop" };
+
+	public static String[][] textAttribs = new String[][] { { "title", "text" }, { "TagName" }, { "Text" } };
+
+	public static final String DATA_STACK = "/data/ghadakcv/stack/";
+
+}
