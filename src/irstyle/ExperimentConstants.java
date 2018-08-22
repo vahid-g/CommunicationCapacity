993ed266517ea5647bd71c5d9be2d0e4660f6c9f
Vahid Ghadakchi <vahid87@gmail.com>
2018-08-21 18:56:30 -0700

    recall constants

--- src/irstyle/ExperimentConstants.java
+++ src/irstyle/ExperimentConstants.java
@@ -9,9 +9,8 @@ public class ExperimentConstants {
 	// best inex p20 sizes. p20 = 0.37 4%, 24%, 4%
 	static int[] precisionLimit = { 597251, 295765, 488317 };
 
-	// best inex recall sizes wth v2 3%, 16%, 55%
-	//static int[] recallLimit = { 400000, 200000, 500000 };
-	static int[] recallLimit = { 597251, 414071, 976634 };
+	// best inex recall sizes wth v2 24%, 59%, 19%
+	static int[] recallLimit = { 2986255, 709836, 1953268};
 	
 	// best msn mrr sizes obtained with v2
 	static int[] mrrLimit = { 238900, 106470, 195326 };
