37e8e4f8775e1007c34c5070c31d837ec4825bb3
vahid-g <vahid87@gmail.com>
2018-08-22 10:13:34 -0700

    PostTags added to schema

--- src/stackoverflow/irstyle/IRStyleStackHelper.java
+++ src/stackoverflow/irstyle/IRStyleStackHelper.java
@@ -8,29 +8,36 @@ import irstyle.DatabaseHelper;
 import irstyle.core.Relation;
 
 public class IRStyleStackHelper {
-	public static Vector<Relation> createRelations(String answersTable, String tagsTable, String commentsTable,
-			Connection conn) throws SQLException {
+	public static Vector<Relation> createRelations(String answersTable, String postTagsTable, String tagsTable,
+			String commentsTable, Connection conn) throws SQLException {
 		// Note that to be able to match qrels with answers, the main table should be
 		// the first relation and the first attrib should be its ID
 		Vector<Relation> relations = new Vector<Relation>();
 		Relation rel = new Relation(answersTable);
 		rel.addAttribute("Id", false, "INTEGER");
+		rel.addAttribute("ParentId", false, "INTEGER");
 		rel.addAttribute("Body", true, "TEXT");
-		rel.addAttr4Rel("Id", tagsTable);
+		rel.addAttr4Rel("ParentId", postTagsTable);
 		rel.addAttr4Rel("Id", commentsTable);
 		rel.setSize(DatabaseHelper.tableSize(answersTable, conn));
 		relations.addElement(rel);
 
+		rel = new Relation(postTagsTable);
+		rel.addAttribute("PostId", false, "INTEGER");
+		rel.addAttribute("TagId", false, "INTEGER");
+		rel.addAttr4Rel("PostId", answersTable);
+		rel.addAttr4Rel("TagId", tagsTable);
+
 		rel = new Relation(tagsTable);
 		rel.addAttribute("Id", false, "INTEGER");
 		rel.addAttribute("TagName", true, "TEXT");
-		rel.addAttribute("ExcerptPostId", false, "INTEGER");
-		rel.addAttr4Rel("ExcerptPostId", answersTable);
+		rel.addAttr4Rel("Id", postTagsTable);
 		rel.setSize(DatabaseHelper.tableSize(tagsTable, conn));
 		relations.addElement(rel);
 
 		rel = new Relation(commentsTable);
 		rel.addAttribute("Id", false, "INTEGER");
+		rel.addAttribute("PostId", false, "INTEGER");
 		rel.addAttribute("Text", true, "TEXT");
 		rel.addAttr4Rel("PostId", answersTable);
 		rel.setSize(DatabaseHelper.tableSize(commentsTable, conn));
