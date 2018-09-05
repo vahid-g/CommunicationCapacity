60a4bf71b196b97b2a86f583ce435f712365cef5
vahid-g <vahid87@gmail.com>
2018-09-05 10:38:05 -0700

    experiment helper refactoring begun

---
+++ src/irstyle/api/ExperimentHelper.java
@@ -0,0 +1,20 @@
+package irstyle.api;
+
+import java.sql.SQLException;
+import java.util.Vector;
+
+import irstyle.core.JDBCaccess;
+import irstyle.core.Relation;
+
+public abstract class ExperimentHelper {
+
+	protected JDBCaccess jdbcAccess;
+
+	public JDBCaccess getJdbcAccess() {
+		return jdbcAccess;
+	}
+
+	abstract public Vector<Relation> createRelations(String firstTable, String secondTable, String thirdTable,
+			String firstRel, String secondRel) throws SQLException;
+
+}
